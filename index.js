0db3f01f14c03bbd5949942e456f10563b3a1478
Bùi Anh Dũng <buianh09dung@gmail.com>
2024-09-07 23:51:01 +0700

    Cấu hình folder routes

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const routeClient = require("./routes/client/index.route");
+
 app.set('views', './views')
 app.set('view engine', 'pug')
 
-app.get('/', (req, res) => {
-  res.render('client/pages/home/index.pug')
-})
-app.get('/products', (req, res) => {
-  res.render('client/pages/products/index')
-})
+routeClient(app);
+
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
